508c74a528697d00b743ddc841987e09ad6f7101
unknown <future870914@gmail.com>
2017-11-13 15:09:50 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int counter=1;
+        int sum=1;
+        while(n<0)
+            n=in.nextInt();
+        while (counter<=n)
+        {
+            sum*=counter;
+            counter++;
+        }
+        System.out.print(sum);
+    }
+}
